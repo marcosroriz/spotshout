d8d9b75a4448bcca6e6dfce7916f7378495f4e78
marcosrorizinf <marcosrorizinf@953e7c3e-8160-c621-384d-5701c90a440b>
2010-10-20 12:57:33 +0000

    Added serialization to each primitive type, to allow reflection and automatic stub generation.

---
+++ SpotShout/src/com/google/code/spotshout/lang/SerialByte.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.google.code.spotshout.lang;
+
+/**
+ *
+ * @author Marcos Roriz
+ */
+public class SerialByte {
+
+}
