c846ff8b85b105675fa0c2da4c69c7bdef52f509
marcosrorizinf <marcosrorizinf@953e7c3e-8160-c621-384d-5701c90a440b>
2010-10-20 14:08:36 +0000

    Changed Project to UTF8

--- SpotShout/src/com/google/code/spotshout/remote/SampleStub.java
+++ SpotShout/src/com/google/code/spotshout/remote/SampleStub.java
@@ -5,12 +5,22 @@
 
 package com.google.code.spotshout.remote;
 
+import com.google.code.spotshout.lang.SerialInt;
+
 /**
  *
  * @author Marcos Roriz
  */
 public class SampleStub extends Stub {
 
+    public int add(int x, int y) {
+        Object[] args = new Object[2];
+        args[0] = new SerialInt(x);
+        args[1] = new SerialInt(x);
+        TargetMethod m = new TargetMethod("add", "(II)I", true, args);
+
+        return 1;
+    }
     public void run() {
         System.out.println("LOL");
     }
