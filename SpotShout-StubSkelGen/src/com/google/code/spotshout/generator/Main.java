2b62f7bde466bbe66b8a484f8be1db0b5994634e
marcosrorizinf <marcosrorizinf@953e7c3e-8160-c621-384d-5701c90a440b>
2010-10-22 01:19:04 +0000

    Added Generator for Stub

---
+++ SpotShout-StubSkelGen/src/com/google/code/spotshout/generator/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.google.code.spotshout.generator;
+
+/**
+ *
+ * @author Marcos Roriz
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        StubGenerator sg = new StubGenerator();
+        sg.makeClass("SpotShout-0.0.1.jar", "com.google.code.spotshout.remote.", "SampleRemIF");
+    }
+}
